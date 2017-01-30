06b550b362cdafd68ec503f837e76aa308d8f387
Aidan Baack <aebaack@gmail.com>
2017-01-30 09:26:27 -0700

    Update knex config

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,14 @@
 
 module.exports = {
 
-  development: { },
-  test: { },
+  development: { 
+    client: 'pg',
+    connection: 'postgres://localhost/classified_dev'
+  },
+  test: { 
+    client: 'pg',
+    connection: 'postgres://localhost/classified_test'
+  },
   production: { },
 
 };
