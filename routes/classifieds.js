8082f2bf8ed1fb08520e284b37666c7b2a56b8c4
Aidan Baack <aebaack@gmail.com>
2017-01-30 09:35:57 -0700

    Add classifieds get route

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -2,9 +2,18 @@
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+router.get('/', (req, res, next) => {
+  knex('classifieds')
+    .select(['id', 'title', 'description', 'price', 'item_image'])
+    .then((classifieds) => {
+      res.json(classifieds);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
